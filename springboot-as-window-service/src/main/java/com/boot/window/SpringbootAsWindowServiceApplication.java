39c987fa7e15eeadb2dffbad10453a47ae058651
nfebrian13 <febrian.nana@gmail.com>
2020-05-17 16:05:28 +0700

    initial commit

---
+++ springboot-as-window-service/src/main/java/com/boot/window/SpringbootAsWindowServiceApplication.java
@@ -0,0 +1,13 @@
+package com.boot.window;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAsWindowServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAsWindowServiceApplication.class, args);
+	}
+
+}
