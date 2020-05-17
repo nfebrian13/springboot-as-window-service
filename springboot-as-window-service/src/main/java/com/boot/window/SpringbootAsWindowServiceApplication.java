90ee241e99285300561e121924ccd4036b8df2dd
nfebrian13 <febrian.nana@gmail.com>
2020-05-17 16:13:06 +0700

    add service

--- springboot-as-window-service/src/main/java/com/boot/window/SpringbootAsWindowServiceApplication.java
+++ springboot-as-window-service/src/main/java/com/boot/window/SpringbootAsWindowServiceApplication.java
@@ -1,13 +1,23 @@
 package com.boot.window;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringbootAsWindowServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootAsWindowServiceApplication.class, args);
+	}
+
+	@Scheduled(fixedRate = 5000)
+	public void printDate() {
+		System.out.println("Hello World service: " + new Date());
 	}
 
 }
